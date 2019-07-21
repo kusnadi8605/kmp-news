4c9c9ec5f09ad20200850f10ec8b92d6976762ea
kusnadi8605 <kusnadi8605@gmail.com>
2019-07-22 06:09:56 +0700

    remove unused script

--- datastruct/ds_news.go
+++ datastruct/ds_news.go
@@ -2,42 +2,10 @@ package datastruct
 
 //News ..
 type News struct {
-	//* tanda bintang supaya tidak err jika datanya null
 	ID      int64
 	Author  string
 	Body    string
 	Created string
-}
-
-//NewsSave ..
-type NewsSave struct {
-	//* tanda bintang supaya tidak err jika datanya null
-	ID int64
-}
-
-//NewsSaveRequest ..
-type NewsSaveRequest struct {
-	//* tanda bintang supaya tidak err jika datanya null
-	Author string `json:"author,omitempty"`
-	Body   string `json:"body,omitempty"`
-}
-
-//NewsSaveResponse ..
-type NewsSaveResponse struct {
-	ResponseCode string     `json:"responseCode"`
-	ResponseDesc string     `json:"responseDesc"`
-	Payload      []NewsSave `json:"payload"`
-}
-
-//NewsDeleteResponse ..
-type NewsDeleteResponse struct {
-	ResponseCode string `json:"responseCode"`
-	ResponseDesc string `json:"responseDesc"`
-}
-
-//NewsRequest ..
-type NewsRequest struct {
-	ID string `json:"id,omitempty"`
 }
 
 //NewsResponse data
@@ -53,16 +21,4 @@ type NewsDetail struct {
 	Author  string
 	Body    string
 	Created string
-}
-
-//NewsDetailRequest ..
-type NewsDetailRequest struct {
-	ID string `json:"id,omitempty"`
-}
-
-//NewsDetailResponse data
-type NewsDetailResponse struct {
-	ResponseCode string       `json:"responseCode"`
-	ResponseDesc string       `json:"responseDesc"`
-	Payload      []NewsDetail `json:"payload"`
 }
